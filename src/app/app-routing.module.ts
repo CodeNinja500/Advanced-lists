fd2b0ebf760a0bb6e2d959d515578ee38410e5cf
Maciej Paluch <maciekpal@gmail.com>
2022-11-13 17:47:53 +0100

    Sorted product list by price
    Products sorted by price displayed under path "/sorted-products"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,19 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { FilteredProductsRoutingComponent } from './components/filtered-products-routing/filtered-products-routing.component';
 import { FilteredProductsSubjectComponent } from './components/filtered-products-subject/filtered-products-subject.component';
+import { SortedProductsListComponent } from './components/sorted-products-list/sorted-products-list.component';
 import { FilteredProductsRoutingComponentModule } from './components/filtered-products-routing/filtered-products-routing.component-module';
 import { ProductsServiceModule } from './services/products.service-module';
 import { CategoriesServiceModule } from './services/categories.service-module';
 import { FilteredProductsSubjectComponentModule } from './components/filtered-products-subject/filtered-products-subject.component-module';
+import { SortedProductsListComponentModule } from './components/sorted-products-list/sorted-products-list.component-module';
 
 @NgModule({
   imports: [RouterModule.forRoot([
     { path: 'products/:category', component: FilteredProductsRoutingComponent },
-    { path: 'products', component: FilteredProductsSubjectComponent }
-  ]), FilteredProductsRoutingComponentModule, ProductsServiceModule, CategoriesServiceModule, FilteredProductsSubjectComponentModule],
+    { path: 'products', component: FilteredProductsSubjectComponent },
+    { path: 'sorted-products', component: SortedProductsListComponent }
+  ]), FilteredProductsRoutingComponentModule, ProductsServiceModule, CategoriesServiceModule, FilteredProductsSubjectComponentModule, SortedProductsListComponentModule],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
